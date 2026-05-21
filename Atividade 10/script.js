e952aaa880e5551e8f0c33f4e1b1853ceab6a240
Arthur Entsev <126127157+arthurlynow098@users.noreply.github.com>
2026-05-21 05:35:52 -0300

    Update script.js

--- Atividade 10/script.js
+++ Atividade 10/script.js
@@ -1,21 +1,90 @@
-class Pessoa {
-  constructor(nome, idade, hobby) {
-    this.nome = nome;
-    this.idade = idade;
-    this.hobby = hobby;
-  }
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formas de criar objetos</title>
+</head>
+<body>
+    <h1>Atividade 10</h1>
+</body>
+<script>
+    class Aluno {
+        constructor(nome, ra, email = null) {
+            this.nome = nome;
+            this.ra = ra;
+            this.email = email;
+        }
 
-  apresentar() {
-    console.log(`Olá, meu nome é ${this.nome}, tenho ${this.idade} anos e o meu hobby é ${this.hobby}.`);
-  }
-}
+        mostrarDados() {
+            alert(`O aluno ${this.nome} tem o RA ${this.ra}`);
+            if (this.email) {
+                alert(this.email);
+            }
+        }
+    }
 
-const p1 = new Pessoa("Arthur Mendes Entsev", 19, "programar");
-const p2 = new Pessoa("Mariana", 21, "fotografia");
-const p3 = new Pessoa("Carlos", 25, "jogar xadrez");
-const p4 = new Pessoa("Beatriz", 19, "pintura");
-const p5 = new Pessoa("Lucas", 22, "andar de bicicleta");
+    class AlunoADS extends Aluno {
+        constructor(nome, ra, numLab) {
+            super(nome, ra);
+            this.numLab = numLab;
+        }
 
-const pessoas = [p1, p2, p3, p4, p5];
+        mostrarDados() {
+            alert(`O aluno ${this.nome} tem o RA ${this.ra} e o número do laboratório é ${this.numLab}`);
+        }
+    }
 
-pessoas.forEach(pessoa => pessoa.apresentar());
+    class ContaBancaria {
+        #cpf;
+
+        constructor(nome, saldoInicial) {
+            this.nome = nome;
+            this.saldo = saldoInicial;
+        }
+
+        setCpf(value) {
+            this.#cpf = value;
+        }
+
+        getCpf() {
+            return this.#cpf;
+        }
+
+        depositar(valor) {
+            this.saldo += valor;
+            this.#registrarTransacao(valor);
+        }
+
+        #registrarTransacao(valor) {
+            console.log(`Deposito de R$ ${valor}, realizado pelo ${this.nome} e o Saldo R$ ${this.saldo} para cpf ${this.getCpf()} ou ${this.#cpf}`);
+        }
+    }
+
+    const alunos = [
+        new Aluno('Arthur Mendes Entsev', '0031432612002'),
+        new Aluno('Enzo', '1234567890', 'enzo@gmail.com'),
+        new Aluno('José', '1234567890'),
+        new Aluno('Lucas', '1234567890'),
+        new Aluno('Pedro', '1234567890'),
+        new Aluno('Pedro', '1234567890'),
+        new AlunoADS('Murilo', '1234567890', '5'),
+        new AlunoADS('Daniel', '1234567890', '5'),
+        new AlunoADS('Richard', '0031432612024', '6')
+    ];
+
+    alunos.forEach((aluno, index) => {
+        alert(`Aluno ${index + 1}`);
+        aluno.mostrarDados();
+    });
+
+    alert(`Cópia de objeto`);
+    const obj1 = new Aluno('Camila', '1234567890');
+    const obj2 = obj1;
+    alert(obj2.nome);
+
+    const conta = new ContaBancaria("Gustavo", 1000);
+    conta.setCpf(123);
+    conta.depositar(500);
+</script>
+</html>
